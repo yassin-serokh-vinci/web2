537b690114e3ed68cdefb842224f0e8d9eceebe5
yassin-serokh-vinci <yassin.serokh@student.vinci.be>
2025-12-04 18:47:23 +0100

    new: ex1.1

---
+++ exercises/1.1/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import filmsRouter from "./routes/films";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/films", filmsRouter);
+
+export default app;
