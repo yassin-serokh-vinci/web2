372efcbcc7fe392dce79e04332979f73fef1b07b
yassin-serokh-vinci <yassin.serokh@student.vinci.be>
2025-12-16 16:18:22 +0100

    new: ex1.7

---
+++ exercises/1.7/types.ts
@@ -0,0 +1,14 @@
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number; 
+  budget?: number;
+  description?: string;
+  imageUrl?: string; 
+}
+
+type NewFilm = Omit<Film, "id">;
+
+export type { Film, NewFilm };
+
