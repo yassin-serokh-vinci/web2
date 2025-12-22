ad608c5379108ab921430227d94d34bdd719e57a
yassin-serokh-vinci <yassin.serokh@student.vinci.be>
2025-12-22 19:50:08 +0100

    new: 2.5-events

--- exercises/2.5/src/Components/App/index.tsx
+++ exercises/2.5/src/Components/App/index.tsx
@@ -4,10 +4,11 @@ import ClickCounter from '../ClickCounter'
 
 const title = "Click Counter";
 const message = "You are a master in the art of clicking !";
+const messageOverFlow = "Please click on me now !"
 function App() {
   return (
     <>
-      <ClickCounter title={title} messageAfter10={message}
+      <ClickCounter title={title} messageAfter10={message} messageOverFlow={messageOverFlow}
       />
     </>
   );
--- exercises/2.5/src/Components/ClickCounter/index.tsx
+++ exercises/2.5/src/Components/ClickCounter/index.tsx
@@ -3,18 +3,26 @@ import { useState } from "react";
 interface ClickCounterProps {
   title: string;
   messageAfter10: string;
+  messageOverFlow: string;
 }
 
-const ClickCounter = ({title, messageAfter10}: ClickCounterProps) => {
+const ClickCounter = ({title, messageAfter10, messageOverFlow}: ClickCounterProps) => {
     const [count, setCount] = useState(0);
+    const [onMouseClick, setOnMouseClick] = useState(false);
+
+    const handleClick = () => {setCount((count) => count + 1)}
+
+    const handleMouseEnter = () => {setOnMouseClick((onMouseClick) => !onMouseClick)}
 
     return (
         <div className="card">
           <h2>{title}</h2>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          
+          {onMouseClick ? <p>{messageOverFlow}</p> : " "}
+          <button onClick= {handleClick} 
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseEnter}
+          >  
+           count is {count}</button>
           {count >=10 && <p>{messageAfter10}</p>}
         </div>
     )
